633eea615d1c60c33957877e325e04300dd1b8bf
eharvey328 <emh90@zips.uakron.edu>
2016-03-30 20:51:44 -0400

    start part3

--- kernel.c
+++ kernel.c
@@ -1,4 +1,6 @@
 ﻿void handleInterrupt21(int,int,int,int);
+void error(int);
+void stop();
 void printString(char*);
 void readString(char*);
 void writeInt(int);
@@ -10,8 +12,9 @@ void readSector(char*, int);
 void readFile(char*, char*, int*);
 void runProgram(char*, int);
 void compareString(char*, char*);
-void error(int);
-void stop();
+void writeSector(char*, int);
+void deleteFile(char*);
+void writeFile(char*,char*,int);
 
 void main()
 {
@@ -30,13 +33,18 @@ void main()
   interrupt(33,0,"Author: Eric Harvey.\r\n\r\n\0",0,0);
 
   /* load fibonacci program */
-
   interrupt(33,4,"fib\0",2,0);
   interrupt(33,0,”"Error if this executes.\r\n\0",0,0);
 
   /* hang the system */
   while(1);
 }
+
+/*******************************************************************************
+*
+*   General Functions
+*
+*******************************************************************************/
 
 void handleInterrupt21(int ax, int bx, int cx, int dx)
 {
@@ -46,12 +54,59 @@ void handleInterrupt21(int ax, int bx, int cx, int dx)
   else if (ax == 3) readFile(bx,cx,dx);
   else if (ax == 4) runProgram(bx,cx);
   else if (ax == 5) stop();
+  else if (ax == 6) writeSector(bx,cx);
+  else if (ax == 7) deleteFile(bx);
+  else if (ax == 8) writeFile(bx,cx,dx);
+  else if (ax == 11) interrupt(25,0,0,0,0); //reboot
   else if (ax == 12) clearScreen(bx, cx);
   else if (ax == 13) writeInt(bx);
   else if (ax == 14) readInt(bx);
   else if (ax == 15) error(bx);
 
   else printString("Invalid interrupt\0");
+}
+
+/* custom error handling */
+void error(int bx)
+{
+   char errMsg0[18], errMsg1[17], errMsg2[13], errMsg3[17];
+
+   errMsg0[0] = 70; errMsg0[1] = 105; errMsg0[2] = 108; errMsg0[3] = 101;
+   errMsg0[4] = 32; errMsg0[5] = 110; errMsg0[6] = 111; errMsg0[7] = 116;
+   errMsg0[8] = 32; errMsg0[9] = 102; errMsg0[10] = 111; errMsg0[11] = 117;
+   errMsg0[12] = 110; errMsg0[13] = 100; errMsg0[14] = 46; errMsg0[15] = 13;
+   errMsg0[16] = 10; errMsg0[17] = 0;
+
+   errMsg1[0] = 66; errMsg1[1] = 97; errMsg1[2] = 100; errMsg1[3] = 32;
+   errMsg1[4] = 102; errMsg1[5] = 105; errMsg1[6] = 108; errMsg1[7] = 101;
+   errMsg1[8] = 32; errMsg1[9] = 110; errMsg1[10] = 97; errMsg1[11] = 109;
+   errMsg1[12] = 101; errMsg1[13] = 46; errMsg1[14] = 13; errMsg1[15] = 10;
+   errMsg1[16] = 0;
+
+   errMsg2[0] = 68; errMsg2[1] = 105; errMsg2[2] = 115; errMsg2[3] = 107;
+   errMsg2[4] = 32; errMsg2[5] = 102; errMsg2[6] = 117; errMsg2[7] = 108;
+   errMsg2[8] = 108; errMsg2[9] = 46; errMsg2[10] = 13; errMsg2[11] = 10;
+   errMsg2[12] = 0;
+
+   errMsg3[0] = 71; errMsg3[1] = 101; errMsg3[2] = 110; errMsg3[3] = 101;
+   errMsg3[4] = 114; errMsg3[5] = 97; errMsg3[6] = 108; errMsg3[7] = 32;
+   errMsg3[8] = 101; errMsg3[9] = 114; errMsg3[10] = 114; errMsg3[11] = 111;
+   errMsg3[12] = 114; errMsg3[13] = 46; errMsg3[14] = 13; errMsg3[15] = 10;
+   errMsg3[16] = 0;
+
+   switch(bx)
+   {
+     case 0: printString(errMsg0); break;
+     case 1: printString(errMsg1); break;
+     case 2: printString(errMsg2); break;
+     default: printString(errMsg3);
+   }
+}
+
+/* to hang the system */
+void stop()
+{
+  while(1);
 }
 
 
@@ -155,7 +210,7 @@ void readInt(int* number)
 
   char line[6];
   readString(line);
-  
+
   /* while not the end of the "integer string" ('0') */
   for (; line[i] != '\0'; ++i)
   {
@@ -207,7 +262,6 @@ int div(int a, int b)
   while (q * b <= a) q++;
   return (q - 1);
 }
-
 
 /*******************************************************************************
 *
@@ -318,45 +372,30 @@ int stringCompare(char* indexed, char* fileName)
 	return match;
 }
 
-/* custom error handling */
-void error(int bx)
+/*******************************************************************************
+*
+*   Part 3: Shell and Writing to Disk
+*
+*******************************************************************************/
+
+void writeSector(char* buffer, int sector)
 {
-   char errMsg0[18], errMsg1[17], errMsg2[13], errMsg3[17];
+  /* convert to relative sector*/
+  int relSecNo = mod(sector,18) + 1;
+  int temp = div(sector,18);
+  int head = mod(temp,2);
+  int track = div(sector,36);
 
-   errMsg0[0] = 70; errMsg0[1] = 105; errMsg0[2] = 108; errMsg0[3] = 101;
-   errMsg0[4] = 32; errMsg0[5] = 110; errMsg0[6] = 111; errMsg0[7] = 116;
-   errMsg0[8] = 32; errMsg0[9] = 102; errMsg0[10] = 111; errMsg0[11] = 117;
-   errMsg0[12] = 110; errMsg0[13] = 100; errMsg0[14] = 46; errMsg0[15] = 13;
-   errMsg0[16] = 10; errMsg0[17] = 0;
-
-   errMsg1[0] = 66; errMsg1[1] = 97; errMsg1[2] = 100; errMsg1[3] = 32;
-   errMsg1[4] = 102; errMsg1[5] = 105; errMsg1[6] = 108; errMsg1[7] = 101;
-   errMsg1[8] = 32; errMsg1[9] = 110; errMsg1[10] = 97; errMsg1[11] = 109;
-   errMsg1[12] = 101; errMsg1[13] = 46; errMsg1[14] = 13; errMsg1[15] = 10;
-   errMsg1[16] = 0;
-
-   errMsg2[0] = 68; errMsg2[1] = 105; errMsg2[2] = 115; errMsg2[3] = 107;
-   errMsg2[4] = 32; errMsg2[5] = 102; errMsg2[6] = 117; errMsg2[7] = 108;
-   errMsg2[8] = 108; errMsg2[9] = 46; errMsg2[10] = 13; errMsg2[11] = 10;
-   errMsg2[12] = 0;
-
-   errMsg3[0] = 71; errMsg3[1] = 101; errMsg3[2] = 110; errMsg3[3] = 101;
-   errMsg3[4] = 114; errMsg3[5] = 97; errMsg3[6] = 108; errMsg3[7] = 32;
-   errMsg3[8] = 101; errMsg3[9] = 114; errMsg3[10] = 114; errMsg3[11] = 111;
-   errMsg3[12] = 114; errMsg3[13] = 46; errMsg3[14] = 13; errMsg3[15] = 10;
-   errMsg3[16] = 0;
-
-   switch(bx)
-   {
-     case 0: printString(errMsg0); break;
-     case 1: printString(errMsg1); break;
-     case 2: printString(errMsg2); break;
-     default: printString(errMsg3);
-   }
+  /* call interrupt to write sector */
+  interrupt(0x13, 796, buffer, track*256+relSecNo, head*256+0);
 }
 
-/* to hang the system */
-void stop()
+void deleteFile(char* name)
 {
-  while(1);
+    return;
+}
+
+void writeFile(char* name, char* buffer, int numOfSectors)
+{
+  return;
 }
